a0f2c616662981efbb2d48b856b625ee10ddb61a
Maksym Vasylenko <maksym.vasylenko@polcode.net>
2022-12-13 21:01:06 -0500

    feat: add types and parser

---
+++ index.ts
@@ -0,0 +1,18 @@
+import {parse} from "csv-parse"
+import fs from 'fs'
+
+const result: (string | Buffer)[] = []
+
+fs.createReadStream('kepler_data.csv')
+  .pipe(parse({
+    comment: '#',
+    columns: true
+  }))
+  .on('data', (data) => {
+    result.push(data)
+  })
+  .on('end', () => {
+    console.log(result)
+  })
+
+
